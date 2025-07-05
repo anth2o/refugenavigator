1fcf31c35276101f7e6aa83c16a515961a148e3f
Antoine Hoorelbeke <ahoorelbeke@gmail.com>
2025-07-05 12:56:58 +0200

    directly use leaflet rather than react-leaflet to have more control: - specific button to draw rectangle - clear previous rectangle when drawing new one

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,33 +1,69 @@
-import { FeatureGroup, MapContainer, TileLayer } from "react-leaflet";
+import L, { type LeafletEvent } from "leaflet";
 import "./Map.css";
 import "leaflet/dist/leaflet.css";
 import "leaflet-draw/dist/leaflet.draw.css";
-import { latLng } from "leaflet";
-import { useState } from "react";
-import { EditControl } from "react-leaflet-draw";
+import "leaflet-draw";
+import { latLng, Polyline } from "leaflet";
+import { useEffect, useRef, useState } from "react";
 import Button from "@mui/material/Button";
 import { downloadGpx } from "../api";
 import type { BoundingBox } from "../types/coordinates";
 import DownloadIcon from "@mui/icons-material/Download";
-import Tooltip from "@mui/material/Tooltip";
 import Stack from "@mui/material/Stack";
 
 window.type = true; // https://github.com/Leaflet/Leaflet.draw/issues/1026#issuecomment-986702652
 
-const center = latLng(44.9, 5.5);
+const initialCenter = latLng(44.9, 5.5);
+const initialZoom = 10;
 
 export const Map = ({ className }: { className?: string }) => {
-  const [selectedBoundingBox, setSelectedBoundingBox] =
-    useState<BoundingBox | null>(null);
+  const [rectangle, setRectangle] = useState<Polyline | null>(null);
   const [waitingForGpx, setWaitingForGpx] = useState(false);
+  const mapRef = useRef<L.DrawMap | null>(null);
+  const drawnItemsRef = useRef<L.FeatureGroup | null>(null);
+  const drawControlRef = useRef<L.Control.Draw | null>(null);
 
-  const onCreated = (e: any) => {
-    const bounds = e.layer.getBounds();
-    setSelectedBoundingBox({
-      northEast: bounds.getNorthEast(),
-      southWest: bounds.getSouthWest(),
+  useEffect(() => {
+    if (mapRef.current) return;
+    const map = L.map("map").setView(initialCenter, initialZoom);
+    L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+    mapRef.current = map;
+
+    const drawnItems = new L.FeatureGroup();
+    map.addLayer(drawnItems);
+    drawnItemsRef.current = drawnItems;
+    const drawControl = new L.Control.Draw({
+      edit: {
+        featureGroup: drawnItems,
+      },
     });
-  };
+    map.addControl(drawControl);
+    drawControlRef.current = drawControl;
+    map.on("draw:created", onCreateDrawing);
+    return () => {
+      map.removeControl(drawControl);
+    };
+  }, []);
+
+  function clearDrawing() {
+    setRectangle(null);
+    drawnItemsRef.current!.clearLayers();
+  }
+
+  function onStartDrawing() {
+    clearDrawing();
+    const drawHandler = new L.Draw.Rectangle(mapRef.current!);
+    drawHandler.enable();
+  }
+
+  function onCreateDrawing(e: LeafletEvent) {
+    drawnItemsRef.current!.addLayer(e.layer);
+    setRectangle(e.layer);
+  }
+
   return (
     <Stack
       alignItems="center"
@@ -35,39 +71,26 @@ export const Map = ({ className }: { className?: string }) => {
       gap={2}
       className={className}
     >
-      <p className="text-center">
-        Select a rectangle on the map with the upper right button
-      </p>
-      <MapContainer center={center} zoom={10}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <FeatureGroup>
-          <EditControl
-            position="topright"
-            onCreated={onCreated}
-            onDeleted={() => setSelectedBoundingBox(null)}
-            draw={{
-              rectangle: true,
-              polygon: false,
-              polyline: false,
-              circle: false,
-              marker: false,
-              circlemarker: false,
-            }}
-          />
-        </FeatureGroup>
-      </MapContainer>
-      <Tooltip title="Download GPX">
+      {/* https://leafletjs.com/examples/quick-start/ */}
+      <div id="map"></div>
+      <Stack direction="row" gap={2}>
+        <Button onClick={onStartDrawing} variant="contained">
+          Draw a rectangle
+        </Button>
         <span>
           <Button
             onClick={async () => {
+              if (!rectangle) return;
               setWaitingForGpx(true);
-              await downloadGpx(selectedBoundingBox!);
+              const bounds = rectangle.getBounds();
+              const boundingBox: BoundingBox = {
+                northEast: bounds.getNorthEast(),
+                southWest: bounds.getSouthWest(),
+              };
+              await downloadGpx(boundingBox);
               setWaitingForGpx(false);
             }}
-            disabled={!selectedBoundingBox}
+            disabled={!rectangle}
             loading={waitingForGpx}
             startIcon={<DownloadIcon />}
             variant="contained"
@@ -75,7 +98,7 @@ export const Map = ({ className }: { className?: string }) => {
             Download GPX
           </Button>
         </span>
-      </Tooltip>
+      </Stack>
     </Stack>
   );
 };
