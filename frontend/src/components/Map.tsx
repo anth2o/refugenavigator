2d5b27d5c116ee41fc5b1fd90c0aadcb782837cb
Antoine Hoorelbeke <ahoorelbeke@gmail.com>
2026-04-04 21:42:24 +0200

    move leaflet code in dedicated hook

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,72 +1,15 @@
 import { downloadGpx } from "../api";
+import { useLeafletMap } from "../hooks/useLeafletMap";
 import type { BoundingBox } from "../types/coordinates";
 import { DownloadSuccessDialog } from "./DownloadSuccessDialog";
-import "./Map.css";
 import { MapControls } from "./MapControls";
 import Stack from "@mui/material/Stack";
-import L, { type LeafletEvent } from "leaflet";
-import { latLng, Polyline } from "leaflet";
-import "leaflet-draw";
-import "leaflet-draw/dist/leaflet.draw.css";
-import "leaflet/dist/leaflet.css";
-import { useEffect, useRef, useState } from "react";
-
-window.type = true; // https://github.com/Leaflet/Leaflet.draw/issues/1026#issuecomment-986702652
-
-const initialCenter = latLng(44.9, 5.5);
-const initialZoom = 10;
+import { useState } from "react";
 
 export const Map = ({ className }: { className?: string }) => {
-  const [rectangle, setRectangle] = useState<Polyline | null>(null);
-  const [isDrawing, setIsDrawing] = useState(false);
   const [waitingForGpx, setWaitingForGpx] = useState(false);
   const [dialogOpen, setDialogOpen] = useState(false);
-  const mapRef = useRef<L.DrawMap | null>(null);
-  const drawnItemsRef = useRef<L.FeatureGroup | null>(null);
-  const drawControlRef = useRef<L.Control.Draw | null>(null);
-
-  useEffect(() => {
-    if (mapRef.current) return;
-    const map = L.map("map").setView(initialCenter, initialZoom);
-    L.tileLayer("https://{s}.tile.osm.org/{z}/{x}/{y}.png", {
-      attribution:
-        '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors',
-      referrerPolicy: "strict-origin-when-cross-origin", // https://wiki.openstreetmap.org/wiki/Referer
-    }).addTo(map);
-    mapRef.current = map;
-
-    const drawnItems = new L.FeatureGroup();
-    map.addLayer(drawnItems);
-    drawnItemsRef.current = drawnItems;
-    const drawControl = new L.Control.Draw({
-      // don't remove following options: for an unknown reason the associated buttons
-      // don't appear on dev mode, but they do appear in the JS bunde if the false aren't
-      // specified
-      draw: {
-        polyline: false,
-        polygon: false,
-        rectangle: false,
-        circle: false,
-        circlemarker: false,
-        marker: false,
-      },
-      edit: {
-        featureGroup: drawnItems,
-        edit: false,
-        remove: false,
-      },
-    });
-    map.addControl(drawControl);
-    drawControlRef.current = drawControl;
-    map.on("draw:created", (e: LeafletEvent) => {
-      drawnItemsRef.current!.addLayer(e.layer);
-      setRectangle(e.layer);
-      setIsDrawing(false);
-    });
-    return () => {
-      map.removeControl(drawControl);
-    };
-  }, []);
+  const { rectangle, isDrawing, toggleDrawing } = useLeafletMap();
 
   return (
     <>
@@ -84,17 +27,7 @@ export const Map = ({ className }: { className?: string }) => {
           isDrawing={isDrawing}
           isReadyForDownload={!!rectangle}
           waitingForGpx={waitingForGpx}
-          onToggleDrawing={() => {
-            drawnItemsRef.current!.clearLayers();
-            if (rectangle) {
-              setIsDrawing(false);
-            } else {
-              setIsDrawing(true);
-              const drawHandler = new L.Draw.Rectangle(mapRef.current!);
-              drawHandler.enable();
-            }
-            setRectangle(null);
-          }}
+          onToggleDrawing={toggleDrawing}
           onDownloadGpx={async () => {
             if (!rectangle) return;
             setWaitingForGpx(true);
---
+++ frontend/src/hooks/useLeafletMap.ts
@@ -0,0 +1,81 @@
+import "./LeafletMap.css";
+import L, { type LeafletEvent } from "leaflet";
+import { latLng, Polyline } from "leaflet";
+import "leaflet-draw";
+import "leaflet-draw/dist/leaflet.draw.css";
+import "leaflet/dist/leaflet.css";
+import { useEffect, useRef, useState } from "react";
+
+window.type = true; // https://github.com/Leaflet/Leaflet.draw/issues/1026#issuecomment-986702652
+
+const initialCenter = latLng(44.9, 5.5);
+const initialZoom = 10;
+
+export const useLeafletMap = () => {
+  const [rectangle, setRectangle] = useState<Polyline | null>(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+  const mapRef = useRef<L.DrawMap | null>(null);
+  const drawnItemsRef = useRef<L.FeatureGroup | null>(null);
+  const drawControlRef = useRef<L.Control.Draw | null>(null);
+
+  useEffect(() => {
+    if (mapRef.current) return;
+    const map = L.map("map").setView(initialCenter, initialZoom);
+    L.tileLayer("https://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors',
+      referrerPolicy: "strict-origin-when-cross-origin", // https://wiki.openstreetmap.org/wiki/Referer
+    }).addTo(map);
+    mapRef.current = map;
+
+    const drawnItems = new L.FeatureGroup();
+    map.addLayer(drawnItems);
+    drawnItemsRef.current = drawnItems;
+    const drawControl = new L.Control.Draw({
+      // don't remove following options: for an unknown reason the associated buttons
+      // don't appear on dev mode, but they do appear in the JS bunde if the false aren't
+      // specified
+      draw: {
+        polyline: false,
+        polygon: false,
+        rectangle: false,
+        circle: false,
+        circlemarker: false,
+        marker: false,
+      },
+      edit: {
+        featureGroup: drawnItems,
+        edit: false,
+        remove: false,
+      },
+    });
+    map.addControl(drawControl);
+    drawControlRef.current = drawControl;
+    map.on("draw:created", (e: LeafletEvent) => {
+      drawnItemsRef.current!.addLayer(e.layer);
+      setRectangle(e.layer);
+      setIsDrawing(false);
+    });
+    return () => {
+      map.removeControl(drawControl);
+    };
+  }, []);
+
+  const toggleDrawing = () => {
+    drawnItemsRef.current!.clearLayers();
+    if (rectangle) {
+      setIsDrawing(false);
+    } else {
+      setIsDrawing(true);
+      const drawHandler = new L.Draw.Rectangle(mapRef.current!);
+      drawHandler.enable();
+    }
+    setRectangle(null);
+  };
+
+  return {
+    rectangle,
+    isDrawing,
+    toggleDrawing,
+  };
+};
